00534876638a5b1c41ad816191dbf4631116efd2
Raphael Daverio <raphael.daverio@gmail.com>
2020-12-22 19:57:50 +0100

    hola que tal

--- app/playground.rb
+++ app/playground.rb
@@ -1,12 +1,7 @@
-students_age = {
-  "Peter" => 24,
-  "Mary" => 25,
-  "George" => 22,
-  "Emma" => 20
-}
-
-puts students_age["Peter"]
-
-students_age.each do |student, age|
-  puts "#{student}'s age is #{age}!"
+class Castle
+  def self.categories
+    return ['Medieval', 'Norman', 'Ancient']
+  end
 end
+
+puts Castle.categories.join(', bitch ass')
